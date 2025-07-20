fe63a5faff3e23d7d2e93a4d51fc0f22c51302b9
Carson Wardlow <86981998+draaaa@users.noreply.github.com>
2025-07-20 02:08:01 -0400

    Create CommandList.sh

---
+++ zsh/scripts/CommandList.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env zsh
+# many of the commands in this list are rudimentary, and while i'm aware of that, i use this command list as a reference
+# dont make fun of me for commands that i cant remember in the moment lol
+
+
+# color names
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+MAGENTA='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+
+BRIGHT_RED='\e[91m'
+BRIGHT_GREEN='\e[92m'
+BRIGHT_YELLOW='\e[93m'
+BRIGHT_BLUE='\e[94m'
+BRIGHT_MAGENTA='\e[95m'
+BRIGHT_CYAN='\e[96m'
+BRIGHT_WHITE='\e[97m'
+
+RESET='\e[0m'
+
+# system commands
+echo -e "${RED}system${RESET}"
+echo -e "  ${BRIGHT_RED}virshstart${RESET}"
+echo -e "    starts the default server necessary to run a vm in ${MAGENTA}virt-manager${RESET}"
+echo -e ""
+echo -e "  ${BRIGHT_RED}virshstop${RESET}"
+echo -e "    stops the default server necessary to run a vm in ${MAGENTA}virt-manager${RESET}"
+echo -e ""
+echo -e "  ${BRIGHT_RED}tldr arg${RESET}"
+echo -e "    returns a basic explanation and (sometimes) a list of args - essentially a better 'command -h'"
+echo -e "    it doesnt work with every command, mostly only default commands that come with linux"
+echo -e ""
+echo -e "  ${BRIGHT_RED}sudo pacman -Syup${RESET}"
+echo -e "    returns the list of targets that would update without performing them"
+echo -e ""
+
+# fun commands
+echo -e "${YELLOW}fun${RESET}"
+echo -e "  ${BRIGHT_YELLOW}pipes${RESET}"
+echo -e "    executes ${MAGENTA}pipes.sh${RESET}, which creates pipes in the terminal"
+echo -e ""
