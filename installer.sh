9ee3ec6dc3030af7e064c1b87e5c209de0b97de9
= <=>
2025-07-21 09:27:47 -0400

    Added untested Debian 12 compatability

--- installer.sh
+++ installer.sh
@@ -1,5 +1,6 @@
 # There's gonna be a bunch of comments that I don't keep and will remove later in this file. I'm using it for the development process.
 
+
 # Ideation
 #   The Goal
 #     Remove the need to manually install apps, transfer configs, and set up profiles that I use daily. 
@@ -7,10 +8,10 @@
 #     tediously install a long list of things that I use, etc
 #
 #   How do we go about this?
-#     1. Identify the package manager <-- CURRENTLY HERE
+#     1. Identify the package manager <-- MOSTLY HERE
 #          honestly, I'm not sure how I should do this yet. I feel that it'd just be more optimal to base it on the package manager, but the 
 #          'source /etc/os-release' method with detecting distros seems to be the most universal. I'll have to do more research. 
-#     2. Install apps
+#     2. Install apps <-- PARTIALLY HERE
 #          Once we have the package manager identified, it should be fairly easy to install the list of packages that I use. 
 #          Something I need to remember, however, is that not all package managers are as up to date. In particular, if I tried to use 
 #          this command with Debian 12 when it was designed for Debian 13, then there would be some packages that are unable to be installed
@@ -18,6 +19,10 @@
 #          I'm not sure how this will be handled yet. Requires more research and ideation.
 #     3. Apply preferred configs to apps
 #          We'll continue ideating later. I don't want to look too far into the future before I barely even have a base to start with.
+
+
+#TODO
+# Test Debian 12 compatability
 
 
 # Check Distro
@@ -30,8 +35,7 @@ elif [ "$ID" = "debian" ]; then
     if [ "$VERSION_ID" = "13" ]; then
         packageManager="apt"
     elif [ "$VERSION_ID" = "12" ]; then
-        echo "Debian 12 is not currently supported"  # Some apt packages that work on Debian 13 like fastetch don't work on Debian 12
-        exit 0
+        packageManager="aptDeb12"  # For now, we can use a separate name for Debian 12 until I can come up with a workaround for the version issues.
     fi
 fi
 
@@ -42,8 +46,18 @@ fi
 # For now, fastfetch will be the main package used for testing. 
 if [ "$packageManager" = "pacman" ]; then
     sudo pacman -Syu
-    sudo pacman -S fastfetch
+    # fastfetch
+        sudo pacman -S fastfetch
+
 elif [ "$packageManager" = "apt" ]; then
-    sudo apt update && sudo apt upgrade
-    sudo apt install fastfetch
+    sudo apt update && sudo apt upgrade -y
+    # fastfetch
+        sudo apt install fastfetch -y
+
+elif [ "$packageManager" = "aptDeb12" ]; then  # Untested
+    sudo apt update && sudo apt upgrade -y
+    # fastfetch
+        wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.48.1/fastfetch-linux-amd64.deb -0 fastfetch.deb # For now, we can just use one version. Maybe look into using the newest version later
+        sudo apt install ./fastfetch.deb
+        rm fastfetch.deb
 fi
